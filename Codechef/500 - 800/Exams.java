489b0bf61390ee7f1ae3c2161bd52a7d6ad47e6b
ㅤVICKY <vickykofficial890@gmail.com>
2026-01-18 19:00:08 +0530

    Solved 519 rating  pbm

---
+++ Codechef/500 - 800/Exams.java
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/EXAMCHEF
+
+import java.util.*;
+import java.util.Scanner;
+
+class Exams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t != 0) {
+            int x = sc.nextInt(), y = sc.nextInt(), z = sc.nextInt();
+            int total = x*y/2;
+            if(total < z) System.out.println("Yes");
+            else System.out.println("No");
+
+            t--;
+        }
+
+    }
+}
