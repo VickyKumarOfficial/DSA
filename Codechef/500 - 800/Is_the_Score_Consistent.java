0fc2fe362a0ed556cb9cda9c8dd415317f2c46f1
ㅤVICKY <vickykofficial890@gmail.com>
2026-01-22 23:23:38 +0530

    Solved 572 rating pbm

---
+++ Codechef/500 - 800/Is_the_Score_Consistent.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Is_the_Score_Consistent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t--> 0) {
+    		int a = sc.nextInt(), b = sc.nextInt();
+    		int c = sc.nextInt(), d = sc.nextInt();
+    		if(c < a || d < b) System.out.println("IMPOSSIBLE");
+    		else System.out.println("POSSIBLE");
+		}
+		
+		sc.close();
+    }
+}
