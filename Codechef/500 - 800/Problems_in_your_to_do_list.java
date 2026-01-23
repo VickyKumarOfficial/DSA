e7805fba86fc8b97385a5d8aaaf335e8724470d1
ㅤVICKY <vickykofficial890@gmail.com>
2026-01-23 09:28:53 +0530

    Solved 580 rating pbm

---
+++ Codechef/500 - 800/Problems_in_your_to_do_list.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Problems_in_your_to_do_list {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int count = 0, rate = 999;
+            for(int i = 1; i <= n; i++) {
+                int pr = sc.nextInt();
+                if(pr > rate) count++;
+            }
+            System.out.println(count);
+        }
+        
+        sc.close();
+    }    
+}
