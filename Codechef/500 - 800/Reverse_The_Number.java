2adb98b77e09da405e45b8b0fd3f888dc3e596f3
ㅤVICKY <vickykofficial890@gmail.com>
2026-01-25 21:31:28 +0530

    Solved 588 rating pbm

---
+++ Codechef/500 - 800/Reverse_The_Number.java
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FLOW007
+
+import java.util.Scanner;
+
+class Reverse_The_Number {
+       public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t--> 0) {
+		    int n = sc.nextInt();
+		    int temp = 0, rev = 0;
+		    while(n != 0) {
+		        temp = n % 10;
+		        rev = temp + rev*10;
+		        n /= 10;
+		    }
+		    System.out.println(rev);
+		}
+		
+		sc.close();
+       }
+}
