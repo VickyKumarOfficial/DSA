4af527c505b370924b355f4c426bafaff8be4042
ㅤVICKY <vickykofficial890@gmail.com>
2026-05-02 21:55:41 +0530

    Solved 627 rating pbm

---
+++ Codechef/500 - 800/Maximise_the_Tastiness.java
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MAXTASTE
+
+import java.util.Scanner;
+
+public class Maximise_the_Tastiness {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t--> 0) {
+		    int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
+		    int fl, sl;
+		    if(a > b) {
+		        fl = a;
+		    } else fl = b;
+		    if(c > d) {
+		        sl = c;
+		    } else sl = d;
+		    System.out.println(fl + sl);
+		}
+        sc.close();
+    }
+}
