e374e7ee362e436dc1fff6acea6ce663cb34b3c5
ㅤVICKY <vickykofficial890@gmail.com>
2026-05-10 23:33:58 +0530

    Solved 711 rating pbm

---
+++ Codechef/500 - 800/Minimum_Number_of_Coins.java
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MINCOINS
+
+import java.util.Scanner;
+
+public class Minimum_Number_of_Coins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t--> 0) {
+		    int n = sc.nextInt(), x = 0;
+		    if(n%5 != 0) System.out.println(-1);
+		    else {
+		        int ans = (n/10) + ((n%10)/5);
+		        System.out.println(ans);
+		    }
+		}
+        sc.close();
+    }
+}
