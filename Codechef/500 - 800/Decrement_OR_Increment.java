facc532b63cdb31f090f566a992174a59753b551
ㅤVICKY <vickykofficial890@gmail.com>
2026-05-11 21:42:51 +0530

    Solved 722 rating pbm

---
+++ Codechef/500 - 800/Decrement_OR_Increment.java
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/DECINC
+
+import java.util.Scanner;
+
+public class Decrement_OR_Increment {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n%4 == 0) n++;
+		else n--;
+		System.out.println(n);
+        sc.close();
+    }
+}
