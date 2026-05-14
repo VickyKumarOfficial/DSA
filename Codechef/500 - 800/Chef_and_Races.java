255d85c1eb7aec9ee05d01ee4a22b8b93d45ee97
ㅤVICKY <vickykofficial890@gmail.com>
2026-05-14 22:16:38 +0530

    Solved 745 rating pbm

---
+++ Codechef/500 - 800/Chef_and_Races.java
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFRACES?tab=statement
+
+import java.util.Scanner;
+
+public class Chef_and_Races {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t--> 0) {
+		    int x = sc.nextInt(), y = sc.nextInt(), a = sc.nextInt(), b = sc.nextInt();
+		    if((x != a) && (x != b) && (y != a) && (y != b)) System.out.println(2);
+		    else if((x != a) && (x != b) && (y != a) && (y == b)) System.out.println(1);
+		    else if((x != a) && (x != b) && (y == a) && (y != b)) System.out.println(1);
+		    else if((x != a) && (x == b) && (y != a) && (y != b)) System.out.println(1);
+		    else if((x == a) && (x != b) && (y != a) && (y != b)) System.out.println(1);
+		    else System.out.println(0);
+		}
+        sc.close();
+    }
+}
